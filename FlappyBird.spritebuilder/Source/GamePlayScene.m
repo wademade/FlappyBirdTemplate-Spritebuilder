16f31409540bc17953d6ad45697a446c75a8971f
Wade Allen <wademade@me.com>
2015-04-22 11:09:44 -0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,13 +14,12 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
-    }
+}
 
 // put new methods here
 - (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
+}
 
 @end
-@end
-
